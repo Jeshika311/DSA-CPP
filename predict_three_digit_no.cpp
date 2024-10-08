ab918d90c96e49ed2213bf8cbd386cb024147561
jeshika311 <jeshikatha@gmail.com>
2024-10-08 06:32:13 +0530

    add new file

---
+++ predict_three_digit_no.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+int x;
+cout<<"Enter value of x:-"<<endl;
+cin>>x;
+
+if(x>99 && x<1000)
+    {
+    cout<<"It is a three digit number"<<endl;
+    }
+else{
+    cout<<"It is not a three digit number"<<endl;
+    }
+}
