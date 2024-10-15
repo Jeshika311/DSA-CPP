72df29c609fa6066eed684774ef7ec9f6e8e518d
jeshika311 <jeshikatha@gmail.com>
2024-10-15 10:40:02 +0530

    add new file

--- calculate_percentage.cpp
+++ calculate_percentage.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
 
     float sub1,sub2,sub3,sub4,sub5;
-
+    {
     cout<<"marks in sub1"<<endl;
     cin>>sub1;
 
@@ -21,6 +21,6 @@ int main(){
 
     float percentage = (sub1+sub2+sub3+sub4+sub5)/5;
     cout<<"your percenatge is :-"<<percentage<<endl;
-
+    }
     return 0;
 }
