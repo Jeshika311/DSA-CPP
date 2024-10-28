3a6de49a210e59a1d2f5f775fea9d9e0cdf924b4
jeshika311 <jeshikatha@gmail.com>
2024-10-28 18:32:50 +0530

    add new file

---
+++ Pattern/solid_rectangle_pattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int row = 3;
+    int column = 5;
+
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=column;j++){
+        cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
