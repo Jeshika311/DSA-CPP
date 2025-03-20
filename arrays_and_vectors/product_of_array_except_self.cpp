e5109e12e61dd8b9c582c8fa7f25870c103cd561
jeshika311 <jeshikatha@gmail.com>
2025-03-20 06:31:34 +0530

    add new file

---
+++ arrays_and_vectors/product_of_array_except_self.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> productExceptSelf(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> ans(n, 1);
+
+    for (int i = 1; i < n; i++)
+    {
+        ans[i] = ans[i - 1] * nums[i - 1];
+    }
+    int suffix = 1;
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix *= nums[i + 1];
+        ans[i] *= suffix;
+    }
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> nums = {-1, 1, 0, -3, 3};
+
+    vector<int> product = productExceptSelf(nums);
+
+    for (int num : product)
+    {
+        cout << num << " ";
+    }
+    return 0;
+}
