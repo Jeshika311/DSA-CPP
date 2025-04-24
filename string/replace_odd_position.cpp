baf12b6eb3e493d2e4f7acaae4b06ba9e6377af7
jeshika311 <jeshikatha@gmail.com>
2025-04-24 06:34:53 +0530

    add new file

---
+++ string/replace_odd_position.cpp
@@ -0,0 +1,25 @@
+// Input a string of size n and update all the odd positions in the string to character ‘#’. Consider 0-based indexing
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    string str;
+    cout << "Enter a string : ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (i % 2 == 0)
+        {
+            str[i] = '#';
+        }
+    }
+
+    cout << "Modified string : " << str << endl;
+    return 0;
+}
